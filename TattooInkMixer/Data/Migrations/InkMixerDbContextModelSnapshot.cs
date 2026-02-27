f2c0d4c64b7d3dd7a9341a84ba2235d961bf954e
Magdalena Gryczka <magdalena.gryczka@gmail.com>
2026-02-27 19:40:33 +0100

    Add EF Core context migration and startup seeding

---
+++ TattooInkMixer/Data/Migrations/InkMixerDbContextModelSnapshot.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+#nullable disable
+
+namespace TattooInkMixer.Data.Migrations;
+
+[DbContext(typeof(InkMixerDbContext))]
+partial class InkMixerDbContextModelSnapshot : ModelSnapshot
+{
+    protected override void BuildModel(ModelBuilder modelBuilder)
+    {
+#pragma warning disable 612, 618
+        modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
+
+        modelBuilder.Entity("TattooInkMixer.Data.ColorTableRecord", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("INTEGER");
+
+                b.Property<string>("Brand")
+                    .HasColumnType("TEXT");
+
+                b.Property<string>("Category")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
+
+                b.Property<string>("Hex")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
+
+                b.Property<string>("Name")
+                    .IsRequired()
+                    .HasColumnType("TEXT");
+
+                b.HasKey("Id");
+
+                b.ToTable("ColorTableEntries");
+            });
+#pragma warning restore 612, 618
+    }
+}
